c869513dfbb933e82934f38428da27f5ca1d6c92
greguretzky <uretsky.zvi@gmail.com>
2026-02-24 14:06:04 +0100

    feat(config): allow unsplash remote images

--- next.config.js
+++ next.config.js
@@ -9,6 +9,15 @@ import { withPayload } from "@payloadcms/next/withPayload";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+};
 
 export default withPayload(withNextIntl(config));
